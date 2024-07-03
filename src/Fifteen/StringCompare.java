ff85d60188690683a02dd481bf153834a4ef2e1f
hyunjinSD <hyenjin09@nate.com>
2024-07-03 22:09:25 +0900

    first commit

--- src/Fifteen/StringCompare.java
+++ src/Fifteen/StringCompare.java
@@ -5,6 +5,7 @@ public class StringCompare {
         StringCompare sample = new StringCompare();
      //   sample.checkString();
         sample.checkCompare();
+        sample.checkCompareTo();
     }
     public void checkString() {
         String text = "You must know String class.";
@@ -13,7 +14,7 @@ public class StringCompare {
     }
     public void checkCompare() {
         String text = "Check value";
-        String text2 = "Check value";
+        String text2 = new String("Check value");
         if(text==text2) {
             System.out.println("text==text2 result is same.");
         } else {
@@ -22,5 +23,17 @@ public class StringCompare {
         if(text.equals("Check value")) {
             System.out.println("text.equals(text2) result is same");
         }
+        String text3="check value";
+        if(text.equalsIgnoreCase(text3)) {
+            System.out.println("text.equalsIgnoreCase(text3) result is same.");
+        }
+    }
+    public void checkCompareTo() {
+        String text = "a";
+        String text2 = "b";
+        String text3 = "c";
+        System.out.println(text2.compareTo(text));
+        System.out.println(text2.compareTo(text3));
+        System.out.println(text.compareTo(text3));
     }
 }
