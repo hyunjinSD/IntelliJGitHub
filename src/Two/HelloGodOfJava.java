1d0d73a47392a3d9bdb29c7540ae1b511be5fb9c
hyunjinSD <hyenjin09@nate.com>
2024-07-05 22:50:20 +0900

    first commit

---
+++ src/Two/HelloGodOfJava.java
@@ -0,0 +1,8 @@
+package Two;
+
+public class HelloGodOfJava {
+    public static void main(String[] args) {
+        System.out.println("Hello God Of Java!!!!!");
+        System.out.println("I love this book.");
+    }
+}
---
+++ src/Two/Profile.java
@@ -0,0 +1,9 @@
+package Two;
+
+public class Profile {
+    public static void main(String[] args) {
+
+        System.out.println("My name is 전현진");
+        System.out.println("MY age is 30");
+    }
+}
