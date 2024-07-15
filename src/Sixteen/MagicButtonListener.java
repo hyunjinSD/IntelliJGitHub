c4cc786f993e6cc5cd82bdf41dc6d230f2db0e01
hyunjinSD <hyenjin09@nate.com>
2024-07-15 23:30:26 +0900

    first commit

---
+++ src/Sixteen/MagicButtonListener.java
@@ -0,0 +1,9 @@
+package Sixteen;
+
+import java.util.EventListener;
+
+public class MagicButtonListener implements EventListener {
+    public void onClick() {
+        System.out.println("Magic Button clicked !!!");
+    }
+}
