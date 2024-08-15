e9a9fa31b2f9d04b62b4023dcad58b6f6cfd42f6
hyunjinSD <hyenjin09@nate.com>
2024-08-15 23:17:21 +0900

    loading

---
+++ src/study/lotto/Loading.java
@@ -0,0 +1,12 @@
+package study.lotto;
+
+public class Loading {
+    public void Loading() {
+        // 프로그램을 주어진 시간동안 대기하도록 하는 처리
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+
+        }
+    }
+}
